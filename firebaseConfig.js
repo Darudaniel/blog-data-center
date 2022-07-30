29ee1c97e28c3336f4317e1c6d42d68726c1cf2e
Daru <danielelrincon@gmail.com>
2022-07-30 17:38:59 -0500

    sec fix

--- firebaseConfig.js
+++
@@ -1,9 +0,0 @@
-export default {
-  apiKey: "AIzaSyAPISnc0KCGVWxdlfwd3KaPyyEpNHeb-xc",
-  authDomain: "blog-data-center.firebaseapp.com",
-  projectId: "blog-data-center",
-  storageBucket: "blog-data-center.appspot.com",
-  messagingSenderId: "418995246916",
-  appId: "1:418995246916:web:8484505401a11de3f32911",
-  measurementId: "G-7GBGJCCRVC"
-};
